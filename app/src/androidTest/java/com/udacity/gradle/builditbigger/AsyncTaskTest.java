64c626fdb14f66a4020610baa17112a77fcedba7
Artur Schaefer <artur.schaefer@hotmail.com>
2018-09-17 19:09:13 -0300

    fix the context in the test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -2,6 +2,7 @@ package com.udacity.gradle.builditbigger;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 
 import com.example.jokesandroid.JokeActivity;
@@ -10,6 +11,7 @@ import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -17,6 +19,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 import static junit.framework.Assert.fail;
 import static org.hamcrest.core.IsNull.notNullValue;
@@ -24,18 +27,25 @@ import static org.junit.Assert.assertThat;
 
 public class AsyncTaskTest{
 
+    private Context mContext;
     @Test
     public void testCallJoke() {
         try {
-            EndpointsAsyncTask task = new EndpointsAsyncTask();
-            task.execute();
-            String joke = task.get(30, TimeUnit.SECONDS);
-
-            assertThat(joke, notNullValue());
-            assertTrue(joke.length() > 0);
-
-        } catch (Exception e) {
-            fail("Operation timed out");
+            assertTrue(true);
+            final CountDownLatch latch = new CountDownLatch(1);
+            mContext = InstrumentationRegistry.getContext();
+            EndpointsAsyncTask testTask = new EndpointsAsyncTask() {
+                @Override
+                protected void onPostExecute(String result) {
+                    assertNotNull(result);
+                    assertTrue(result.length() > 0);
+                    latch.countDown();
+                }
+            };
+            testTask.execute(mContext);
+            latch.await();
+        } catch (Exception e){
+            fail("Get Exception Test - " + e.toString());
         }
     }
 }
